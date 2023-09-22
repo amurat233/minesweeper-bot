6a8b758f91b10ceab0934420d5bfd02b1572a430
amurat233 <alikhanmurat05@gmail.com>
2023-09-22 14:54:31 +0800

    made a bot controller class. gameboard broken :(

---
+++ botcontroller.py
@@ -0,0 +1,47 @@
+from gameboard import GameBoard
+
+class BotController:
+    def __init__(self, board: GameBoard):
+        self.board = board
+
+    def mark_basic_flags(self):
+        board = self.board.get_board()
+        for r, row in enumerate(board):
+            for c, elem in enumerate(row):
+                if elem == -1 or elem == 9:
+                    continue
+                
+                num_mines = elem
+                num_flagged = self.__count_flags(r, c)
+
+                if num_flagged == num_mines:
+                     for x_off, y_off in [(1,1), (1,0), (1,-1), (0,1), (0,-1), (-1,1), (-1,0), (-1,-1)]:
+                        if (r + y_off >= 0 and r + y_off < self.board.rows) and (c + x_off >= 0 and c + x_off < self.board.cols):
+                            if board[r+y_off][c+x_off] == -1:
+                                self.board.reveal(r+y_off,c+x_off)
+
+                num_hidden = self.__count_hidden(r,c)
+                if num_hidden == num_mines:
+                    for x_off, y_off in [(1,1), (1,0), (1,-1), (0,1), (0,-1), (-1,1), (-1,0), (-1,-1)]:
+                        if (r + y_off >= 0 and r + y_off < self.board.rows) and (c + x_off >= 0 and c + x_off < self.board.cols):
+                            if board[r+y_off][c+x_off] == -1:
+                                self.board.flag(r+y_off,c+x_off)
+        
+    # returns (num_hidden, num_flags)
+    def __count_hidden(self, row, col):
+        board = self.board.get_board()
+        num_hidden = 0
+        for x_off, y_off in [(1,1), (1,0), (1,-1), (0,1), (0,-1), (-1,1), (-1,0), (-1,-1)]:
+                if (row + y_off >= 0 and row + y_off < self.board.rows) and (col + x_off >= 0 and col + x_off < self.board.cols):
+                    if board[row + y_off][col + x_off] == -1:
+                        num_hidden += 1
+        return num_hidden
+    
+    def __count_flags(self, row, col):
+        board = self.board.get_board()
+        num_flags = 0
+        for x_off, y_off in [(1,1), (1,0), (1,-1), (0,1), (0,-1), (-1,1), (-1,0), (-1,-1)]:
+                if (row + y_off >= 0 and row + y_off < self.board.rows) and (col + x_off >= 0 and col + x_off < self.board.cols):
+                    if board[row + y_off][col + x_off] == 9:
+                        num_flags += 1
+        return num_flags
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import sys
 from guidisplay import GUIDisplay
 from gameboard import GameBoard
+from botcontroller import BotController
+from textdisplay import TextDisplay
 import pygame
 
 if __name__ == "__main__":
     pygame.init()
     rows, cols, mines, grid_size = (int(i) for i in sys.argv[1:])
     board = GameBoard(rows, cols, mines)
+    tdisplay = TextDisplay(board=board)
     display = GUIDisplay(board=board, grid_size=grid_size)
+    controller = BotController(board)
 
     running = True
     while running:
@@ -17,15 +21,20 @@ if __name__ == "__main__":
                 quit()
                 
         display.update()
-
-        move = input("Flag (F) or Reveal (R)?")
-        row = int(input("Row:"))
-        col = int(input("Col:"))
+        tdisplay.update()
+        
+        move = input("Flag (F) or Reveal (R) or Basic (B)?")
 
         if move.lower() == "f":
+            row = int(input("Row:"))
+            col = int(input("Col:"))
             board.flag(row, col)
         elif move.lower() == "r":
+            row = int(input("Row:"))
+            col = int(input("Col:"))
             board.reveal(row, col)
+        elif move.lower() == "b":
+            controller.mark_basic_flags()
         else:
             running = False
             pygame.quit()
